ffdf04ee56cd19cbece003d2a5c510736458c906
kinga00099 <kinga00099@gmail.com>
2018-11-05 13:47:08 +0100

    new enemy added with default moving

--- src/com/codecool/snake/Globals.java
+++ src/com/codecool/snake/Globals.java
@@ -32,6 +32,7 @@ public class Globals {
         resources.addImage("SnakeHead", new Image("snake_head.png"));
         resources.addImage("SnakeBody", new Image("snake_body.png"));
         resources.addImage("SimpleEnemy", new Image("simple_enemy.png"));
+        resources.addImage("EpicEnemy", new Image("bloody_knife.png"));
         resources.addImage("PowerUpBerry", new Image("powerup_berry.png"));
     }
 
