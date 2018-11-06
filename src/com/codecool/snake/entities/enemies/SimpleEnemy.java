4561915aeb718d272cd6b7a60a7b484a177a63b9
dr1zzt2 <dr1zzt2.NK@gmail.com>
2018-11-06 14:37:01 +0100

    fix for enemy spawn

--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -21,9 +21,13 @@ public class SimpleEnemy extends Enemy implements Animatable, Interactable {
         super(10);
 
         setImage(Globals.getInstance().getImage("SimpleEnemy"));
+        double posX = rnd.nextDouble() * Globals.WINDOW_WIDTH;
+        double posY = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
+        setX(posX);
+        setY(posY);
         while (Globals.getInstance().display.getObjectList().get(0).getPosition() == this.getPosition()) {
-            double posX = rnd.nextDouble() * Globals.WINDOW_WIDTH;
-            double posY = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
+            posX = rnd.nextDouble() * Globals.WINDOW_WIDTH;
+            posY = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
             setX(posX);
             setY(posY);
         }
